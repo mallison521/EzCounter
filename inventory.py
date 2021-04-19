3d0ab4ad581868c339fd689738366d46b247ed3d
mallison521 <51099954+mallison521@users.noreply.github.com>
2021-04-19 11:33:56 -0400

    Added two new functions to create a new inventory item and update a current one

--- inventory.py
+++ inventory.py
@@ -11,19 +11,30 @@ inventory = {
 
 def removeInventoryItem(amount, item):
     inventory[item] -= amount
-    print(inventory)
+    return inventory
 
-def addInventoryItem(amount, item):
-    inventory[str(item)] = amount
-    if inventory[str(item)] >= 0:
+def newInventoryItem(amount, item):
+    inventory[item] = 0
+    if inventory[item] == 0:
         inventory[item] += amount
-    print(inventory)
+    return inventory
+
+def increaseInventoryItem(amount,item):
+    inventory[item] += amount
+    return inventory
 
 def writeToFile():
     f = csv.writer(open("inventory.csv", "w"))
     for key, val in inventory.items():
         f.writerow([key, val])
 
+def readCSVinventory():
+    with open('inventory.csv') as f:
+        inventory = dict(filter(None, csv.reader(f)))
+    print(inventory)
+
 removeInventoryItem(3, 'bananas')
-addInventoryItem(5, 'loaves of bread')
+newInventoryItem(5, 'loaves of bread')
+increaseInventoryItem(3, 'apples')
 writeToFile()
+readCSVinventory()

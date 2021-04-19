d3bd0f8173bb2d344db04bfd4af3ab2c64176968
mallison521 <51099954+mallison521@users.noreply.github.com>
2021-04-19 09:56:48 -0400

    Added a function to write the inventory to a csv file and add items to inventory

--- inventory.py
+++ inventory.py
@@ -1,4 +1,5 @@
 # an inventory structure
+import csv
 
 inventory = {
             'apples': 2,
@@ -8,8 +9,21 @@ inventory = {
             'sticks of butter': 3
 }
 
-def removeInventoryItem(amount,item):
+def removeInventoryItem(amount, item):
     inventory[item] -= amount
     print(inventory)
 
+def addInventoryItem(amount, item):
+    inventory[str(item)] = amount
+    if inventory[str(item)] >= 0:
+        inventory[item] += amount
+    print(inventory)
+
+def writeToFile():
+    f = csv.writer(open("inventory.csv", "w"))
+    for key, val in inventory.items():
+        f.writerow([key, val])
+
 removeInventoryItem(3, 'bananas')
+addInventoryItem(5, 'loaves of bread')
+writeToFile()

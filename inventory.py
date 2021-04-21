33ae3c2e01fbbad88e5d580c3173006c8e4c1221
mrusinek3 <70675477+mrusinek3@users.noreply.github.com>
2021-04-21 14:15:06 -0400

    Added function to check item count of item's that are currently in the inventory

--- inventory.py
+++ inventory.py
@@ -24,6 +24,17 @@ def writeToFile():
     for key, val in inventory.items():
         f.writerow([key, val])
 
+def checkItemCount(item):
+    for item in inventory:
+        if inventory[item] == 0:
+            print(item,": Item is out of stock! Item needs to be restocked!")
+            print("------------------------------------------------------------------------")
+        elif inventory[item] > 0:
+            print(item,": Item in stock! Remaining item count =", inventory[item])
+            print("------------------------------------------------------------------------")
+
+item = " "
+checkItemCount(item)
 removeInventoryItem(3, 'bananas')
 addInventoryItem(5, 'loaves of bread')
 writeToFile()
